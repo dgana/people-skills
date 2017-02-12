909a4f3659d65ad752900d03b3d81425a575cdc7
Ida Bagus Chahya Dhegana <dhegana@gmail.com>
2017-02-12 17:00:50 +0700

    done back end

--- controllers/user.js
+++ controllers/user.js
@@ -16,15 +16,26 @@ module.exports = {
   },
   add: (req, res) => {
     let objSkill = JSON.parse(req.body.skills)
-    Users.create({
-      name: req.body.name,
-      skills: objSkill,
-      created_at: new Date(),
-      updated_at: new Date()
-    }, (err, user) => {
-      if (err) throw err
-      res.json(user)
+    let getSkill = objSkill.map((user, index) => {
+      return user.skill
     })
+    function checkDuplicate (getSkill) {
+      return getSkill.length === new Set(getSkill).size
+    }
+    // res.send(checkDuplicate(getSkill))
+    if (checkDuplicate(getSkill)) {
+      Users.create({
+        name: req.body.name,
+        skills: objSkill,
+        created_at: new Date(),
+        updated_at: new Date()
+      }, (err, user) => {
+        if (err) throw err
+        res.json(user)
+      })
+    } else {
+      res.json({msg: 'You cannot have duplicate skill!' })
+    }
   }, // [{"skill":"eat", "value": 100},{"skill":"sleep","value": 50}]
   readAll: (req, res) => {
     Users.find({}, (err, users) => {
@@ -59,5 +70,44 @@ module.exports = {
         if (err) throw err
         res.json(result)
       })
+  },
+  addSkills: (req, res) => {
+    let objSkill = JSON.parse(req.body.skills)
+    Users.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: {
+          skills: {
+            $each: objSkill,
+            $sort: {
+              value: -1
+            }
+          }
+        }
+      }, {new: true}, (err, result) => {
+        if (err) throw err
+        res.json(result)
+      })
+  },
+  removeSkill: (req, res) => {
+    Users.findByIdAndUpdate(
+      req.params.id,
+      {
+        $pull: {
+          skills: {
+            skill: req.body.skill
+          }
+        }
+      }, {new: true}, (err, result) => {
+        if (err) throw err
+        res.json(result)
+      })
+  },
+  seedKey: (req, res, next) => {
+    if (req.header('auth') === 'admin123') {
+      next()
+    } else {
+      res.send('You dont have access!')
+    }
   }
 }
