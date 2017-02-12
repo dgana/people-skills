1f7155754e014b1b64b438416457a3510dc42d8b
Ida Bagus Chahya Dhegana <dhegana@gmail.com>
2017-02-12 20:33:33 +0700

    done ajax get

---
+++ public/js/init.js
@@ -0,0 +1,7 @@
+;(function ($) {
+  $(function () {
+    $('.button-collapse').sideNav()
+    $('.modal').modal()
+    $('select').material_select()
+  }) // end of document ready
+})(jQuery) // end of jQuery name space
