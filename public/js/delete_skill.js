d01590f407389440312d1d268026804d64c99ecb
Ida Bagus Chahya Dhegana <dhegana@gmail.com>
2017-02-14 16:50:33 +0700

    done backend frontend people skill

--- public/js/delete_skill.js
+++ public/js/delete_skill.js
@@ -1,3 +1,16 @@
-function delete_skill(userID, i, k) {
-  console.log(userID, i, k);
+function delete_skill(id, i, k) {
+  console.log(id, i, k);
+  let getSkill = $(`#skill_${i}_${k}`).html()
+  console.log(getSkill);
+  $.ajax({
+    type: 'PUT',
+    url: 'http://localhost:3000/api/remove_skill/'+id,
+    data: {skill: getSkill},
+    dataType: 'json',
+    success: function(){
+      $(`#delete_${i}_${k}`).remove()
+      $(`#skill_${i}_${k}`).remove()
+      $(`#value_${i}_${k}`).remove()
+    }
+  })
 }
